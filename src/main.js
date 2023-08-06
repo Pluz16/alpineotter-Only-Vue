395a7824cd1fafb5c33ea21a4622346fb9a6611c
Francesco Punta <puntafrancesco@gmail.com>
2023-08-06 23:24:08 +0200

    bootstrap

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import '@/global.css';
 
